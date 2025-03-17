20d4cd8760c7488e525f8dd4776b83f7d8dd44d3
Johannes Breitfeld <jbvbreitfeld@gmail.com>
2025-03-17 14:09:08 +0100

    Add navbar.js (#2)

---
+++ JS/navbar.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPath = window.location.pathname;
+  
+    const navElement = document.createElement("nav");
+    navElement.className = "navbar navbar-expand-lg bg-dark sticky-top navbar-dark";
+    navElement.innerHTML = `
+    <div class="container-fluid">
+      <a class="navbar-brand m-3" href="../index.html">Bright Swahn Flower Agency<img src="../Media/Images/logo.png style="width:50" alt="BSF" /></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav nav-fill fs-4">
+          <li class="nav-item">
+            <a class="nav-link mx-lg-4" href="../index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-lg-4" href="../pages/About.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active mx-lg-4" href="../pages/Showcase.html">Portfolio</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+      `;
+  
+    document.body.prepend(navElement);
+  
+    const navLinks = document.querySelectorAll(".navbar-nav a");
+  
+    navLinks.forEach((link) => {
+      const linkPath = new URL(link.href).pathname;
+      if (linkPath === currentPath) {
+        link.classList.add("active");
+        link.setAttribute("aria-current", "page");
+      }
+    });
+  });
